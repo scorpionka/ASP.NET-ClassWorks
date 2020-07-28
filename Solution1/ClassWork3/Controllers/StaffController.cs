3fa97af2ee3b16f0287ea91c08916e9cf376530d
Alexey <mgpp@tut.by>
2020-07-28 16:23:00 +0300

    commit 1

---
+++ Solution1/ClassWork3/Controllers/StaffController.cs
@@ -0,0 +1,30 @@
+﻿using ClassWork3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassWork3.Controllers
+{
+    public class StaffController : Controller
+    {
+        public static List<Staff> personnel = new List<Staff>()
+        {
+            new Staff(){FirstName = "Ivan", LastName = "Ivanov", Patronymic = "Ivanovich", BirthDay = },
+            new Staff(){FirstName = "Petr", LastName = "Petrov", Patronymic = "Petrovich"},
+            new Staff(){FirstName = "Alexander", LastName = "Alexandrov", Patronymic = "Alexandrovich"},
+        };
+
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Staff()
+        {
+            return View(personnel);
+        }
+    }
+}
---
+++ Solution1/ClassWork3/Models/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassWork3.Models
+{
+    public class Staff
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
