903f67d8bd2a278619ed2e1fa329587453417aa9
Алексей Кузьмицкий <mgpp@tut.by>
2020-08-04 21:48:36 +0300

    commit

---
+++ Solution1/ClassWork5/Controllers/TemplateController.cs
@@ -0,0 +1,20 @@
+﻿using ClassWork5.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassWork5.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult Index()
+        {
+            var model = new DateTimeView() { };
+            return View(model);
+        }
+    }
+}
---
+++ Solution1/ClassWork5/Models/DateTimeView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClassWork5.Models
+{
+    public class DateTimeView
+    {
+        [UIHint("datetime")]
+        public DateTime dateTime { get; set; }
+    }
+}
