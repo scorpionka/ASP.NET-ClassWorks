bc844090559e07ade69ede55fcfd2bbc2e3ba5fb
Алексей Кузьмицкий <mgpp@tut.by>
2020-08-18 07:47:18 +0300

    doing CW7

---
+++ Solution1/ClassWork7/Controllers/FilterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassWork7.Controllers
+{
+    public class FilterController : Controller
+    {
+        public ActionResult Sum(int a, int b)
+        {
+            return Json(new Sum { Value = a + b });
+        }
+    }
+}
