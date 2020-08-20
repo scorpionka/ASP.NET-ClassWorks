054c648b6c7345d052494567b81e9b49f75ddd4a
Алексей Кузьмицкий <mgpp@tut.by>
2020-08-20 07:51:01 +0300

    CW7 part2 doing

--- Solution1/ClassWork7/Controllers/FilterController.cs
+++ Solution1/ClassWork7/Controllers/FilterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassWork7.Infrastructure.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace ClassWork7.Controllers
 {
     public class FilterController : Controller
     {
+        [CustomResultFilter]
         public ActionResult Index()
         {
             return View();
---
+++ Solution1/ClassWork7/Infrastructure/Filters/CustomResultFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassWork7.Infrastructure.Filters
+{
+    public class CustomResultFilter : FilterAttribute, IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Response.Write("Copyright");
+        }
+    }
+}
