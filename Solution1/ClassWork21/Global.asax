7b1982bf47075c746ac9b5e6819fcc21b6da687f
Алексей Кузьмицкий <mgpp@tut.by>
2020-09-29 21:06:12 +0300

    begin

---
+++ Solution1/ClassWork21/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassWork21.MvcApplication" Language="C#" %>
---
+++ Solution1/ClassWork21/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassWork21.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int NumberOfChildren { get; set; }
+        public int DateOfBirth { get; set; }
+        public string Comments { get; set; }
+        public string Email { get; set; }
+    }
+}
